5e9c7e406d298066e082b9c40da3662499042039
gackt <gackt5251@gmail.com>
2022-12-30 22:15:31 +0900

    データベースに接続するモジュール

---
+++ module/MC-01.php
@@ -0,0 +1,17 @@
+<?PHP
+function getDB() :PDO {
+    $dsn ='mysql:dbname=test; host=localhost; charset=utf8';
+    $usr = 'root';
+    $passwd = '';
+
+   // $db = mysqli_connect($host, $usr, $passwd, $dbname);
+
+   try {
+        // データベースへの接続を確立
+        $db = new PDO($dsn, $usr, $passwd);
+        return $db;
+    } catch(PDOException $e) {
+        echo 'DB接続エラー:'.$e->getMessage();
+        return $db;
+    }
+}
