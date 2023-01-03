46d3dba68a1cfda3eac1d65276be5485d211a712
china1206 <240371i@ugs.kochi-tech.ac.jp>
2023-01-03 22:49:27 +0900

    E-AC3.php,css作成

---
+++ EventUser/php/E-AC3.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>画面ID E-AC3</title>
+  <meta charset=”UTF-8″>
+  <link rel="stylesheet" href="E-AC3.css" type="text/css">
+  <link rel="stylesheet" href="E-menu.css" type="text/css">
+</head>
+<body>
+  <main id="main">
+  <h3>アカウント</h3>
+
+    <div class="parent">
+      <div class="center">アカウント名</div>
+      <div class="left"><p class="image-circle"></p></div>
+    </div>
+    
+    <center>
+      <br>
+      <div class="box">
+        一言コメント
+      </div>
+      <br><a href="E-AC7.html">プロフィール編集</a>
+    </center>
+    <a href="E-AC7.html"><p class="acount-menu">アカウント情報変更</p></a>
+    <a href="E-AC5.html"><p class="acount-menu">アカウントログアウト</p></a>
+    <a href="E-AC6.html"><p class="acount-menu">アカウント消去</p></a>
+</main>
+
+<aside id="sub">
+  
+  <ul>
+    <li class="menu-list"><a class="menu-button" href="E-EL1.html"><img src="E-menu-home.png" width="45" height="43">　ホーム</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="E-CB1.html"><img src="E-menu-post.png" width="45" height="43">　イベント投稿</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="E-SE1.html"><img src="E-menu-see.png" width="45" height="43">　投稿イベント<br>　　　一覧・消去</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="E-AC3.html"><img src="E-menu-acount.png" width="45" height="43">　アカウント</a></li><br>
+  </ul>
+</aside>
+</body>
