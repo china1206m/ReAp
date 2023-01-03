91cd95d587b009e436fd6450268e5832237fcc1f
china1206 <240371i@ugs.kochi-tech.ac.jp>
2023-01-03 22:19:31 +0900

    E-AC5.php,css作成

---
+++ EventUser/php/E-AC5.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>画面ID E-AC5</title>
+  <meta charset=”utf-8″>
+  <link rel="stylesheet" href="E_button.css" type="text/css">
+  <link rel="stylesheet" href="E-AC5.css" type="text/css">
+  <link rel="stylesheet" href="E_position.css" type="text/css">
+</head>
+
+<body bgcolor="#f0f8ff" >
+    <center>
+        
+        
+    <ul>
+        <li><div class="box">
+        <h3>ログアウトしてもよろしいですか？</h3>
+    </div></li>
+    <br><br><br>
+    
+    <li><div>
+    <button type="submit"class="button-yes" onclick="location.href='E-AC4.html'">はい</button>
+  
+   <button type="submit"class="button-yes" onclick="location.href='E-AC3.html'">いいえ</button>
+    </div></li>
+  </ul>
+</center>
+</body>
