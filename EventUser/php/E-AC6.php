43e0792acdcd76bd7cd85c92f1d8b45573f4410c
china1206 <240371i@ugs.kochi-tech.ac.jp>
2023-01-03 22:15:33 +0900

    E-AC6.php,css作成

---
+++ EventUser/php/E-AC6.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>画面ID E-AC6</title>
+  <meta charset=”utf-8″>
+  <link rel="stylesheet" href="E_button.css" type="text/css">
+  <link rel="stylesheet" href="E-AC6.css" type="text/css">
+  <link rel="stylesheet" href="E_position.css" type="text/css">
+</head>
+
+<body bgcolor="#f0f8ff" >
+    <center>
+        <ul>
+
+    <li><div class="box">
+        <h2>アカウント消去規約</h2><br>
+        株式会社ReAp(以下「当社」と記載します。)が提供する計画、イベント投稿システム「Aplan」の利用について同意していただく事項について記載しています。
+        <h4>第一条　定義</h4>
+        <p>1.「本システム」とは、</p>
+        <p>2.「利用者」とは、本システムの利用規約に同意し、第三条において会員登録を行った地方自治体をさします。</p>
+        <h4>第二条　利用</h4>
+        <h4>第三条　利用</h4>
+        <h4>第四条　利用</h4>
+        <h4>第五条　利用</h4>
+        <h4>第六条　利用</h4>
+        <h4>第七条　利用</h4>
+        <h4>第八条　利用</h4>
+        <h4>第九条　利用</h4>
+        <h4>第十条　利用</h4>
+        <h4>第十一条　利用</h4>
+    </div></li>
+<br><br><br>
+<li><div>
+    <button type="submit"class="button-yes" onclick="location.href='E-AC1.html'">同意する</button>
+
+    <button type="submit"class="button-yes" onclick="location.href='E-AC3.html'">同意しない</button>
+</div></li>
+</ul>
+</center>
+</body>
