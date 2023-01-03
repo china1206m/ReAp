6021e88b4b7d718b3baafd88e8a461b65660a12f
gackt <gackt5251@gmail.com>
2023-01-03 22:51:16 +0900

    データベース削除モジュール

---
+++ module/MD.php
@@ -0,0 +1,31 @@
+<?php
+
+class testMD {
+
+    // テーブル名、カラム名、削除したい値、型   
+    function md($table, $columnnaem, $post, $type) : void {
+        try {
+            include "MC-01.php";
+            // データベース接続
+            $db = getDB();
+
+            $sum;
+            if($type == 0) { 
+                $sum = $sum.$post;
+            } else if($type == 1) {
+                $sum = $sum."'".$post."'";
+            } else {
+                $sum = $sum."N'".$post."'";
+            }
+
+            // SQL文をセット
+            $stt = $db->prepare("DELETE FROM ".$table." WHERE ".$columnnaem."=".$sum);
+    
+            // INSERT命令を実行
+            $stt->execute();
+        } catch(PDOException $e) {
+            die("エラーメッセージ：{$e->getMessage()}");
+        }
+    }
+}
+ 
