a95761121d97c789d0c91f55c572477a69423c86
china1206 <240371i@ugs.kochi-tech.ac.jp>
2023-01-06 02:18:39 +0900

    U-AC3.php,css作成

---
+++ User/php/U-AC3.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>画面ID E-AC3</title>
+  <meta charset=”UTF-8″>
+  <link rel="stylesheet" href="U-AC3.css" type="text/css">
+  <link rel="stylesheet" href="U-menu.css" type="text/css">
+</head>
+<body>
+  <main id="main">
+  <h3>アカウント</h3>
+
+    <div class="parent">
+      <div class="center">アカウント名</div>
+      <div class="left"><p class="image-circle"></p></div>
+    </div>
+    
+    <center>
+      <br>
+      <div class="box">
+        一言コメント
+      </div>
+      <br><a class="link" href="U-AC4.php">プロフィール編集</a>
+    </center>
+    <a class="link" href="U-AC5.php"><p class="acount-menu">アカウント情報変更</p></a>
+    <a class="link" href="U-AC7.php"><p class="acount-menu">アカウントログアウト</p></a>
+    <a class="link" href="U-AC8.php"><p class="acount-menu">アカウント消去</p></a>
+</main>
+
+<aside id="sub">
+  <ul class="menu">
+        <li class="menu-list"><a class="menu-button" href="U-HK1.php"><img src="U-menu-home.png" width="47" height="43">　ホーム</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="U-PL1.php"><img src="U-menu-place.png" width="47" height="43">　名所</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="U-EV1.php"><img src="U-menu-event.png" width="47" height="43">　イベント</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="U-FV1.php"><img src="U-menu-favorite.png" width="47" height="43">　お気に入り</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="U-AC3.php"><img src="U-menu-acount.png" width="47" height="43">　アカウント</a></li><br>
+    </ul>
+</aside>
+</body>
