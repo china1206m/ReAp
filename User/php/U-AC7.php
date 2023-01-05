4d62e1f6261cba7f6c773b27fc461b24bf5021c8
china1206 <240371i@ugs.kochi-tech.ac.jp>
2023-01-06 02:47:08 +0900

    U-AC7.php,css作成

---
+++ User/php/U-AC7.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>画面ID U-AC7</title>
+  <meta charset=”UTF-8″>
+  <link rel="stylesheet" href="U-AC7.css" type="text/css">
+  <link rel="stylesheet" href="button.css" type="text/css">
+</head>
+<body bgcolor="#f0f8ff">
+  
+        <center>
+            
+            
+        <ul>
+            <li><div class="box">
+            <h3>ログアウトしてもよろしいですか？</h3>
+        </div></li>
+        <br><br><br>
+        
+        <li><div>
+        <form action="U-AC6.php" method="POST">
+        <button type="submit"class="button-yes">はい</button>
+        </form>
+      
+       <button type="submit"class="button-yes" onclick="location.href='U-AC3.php'">いいえ</button>
+        </div></li>
+      </ul>
+    </center>
+
+
+
+</body>
