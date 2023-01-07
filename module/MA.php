318a76bf6624a64f597ac93102adac9889152247
gackt <gackt5251@gmail.com>
2023-01-07 22:56:41 +0900

    返り値としてIDを返す処理を追加

--- module/MA.php
+++ module/MA.php
@@ -1,7 +1,46 @@
 <?php
 class MA {
+        // テーブル名、追加したい値、追加したい値の型
+        function ma($table, $column, $post, $type) : void {
+            try {
+                // データベース接続
+                include "MC-01.php";
+                $db = getDB();
+    
+                $sum_column = null; // 最終挿入値column
+                $sum_post= null; // 最終挿入値post
+    
+                for($i = 0; $i < count($post); $i++) {
+                    // 複数挿入の場合
+                    if($i >= 1){
+                        $sum_column = $sum_column.",";
+                        $sum_post = $sum_post.",";
+                    }
+                    // column
+                    $sum_column = $sum_column.$column[$i];
+                    // 型調べ
+                    // INSERT
+                    if($type[$i] == 0) {
+                        $sum_post = $sum_post.$post[$i];
+                        // CHAR
+                    } else if($type[$i] == 1) {
+                        $sum_post = $sum_post."'".$post[$i]."'";
+                        // NCHAR
+                    } else {
+                        $sum_post = $sum_post."N'".$post[$i]."'";
+                    }
+                }
+                // SQL文をセット
+                $stt = $db->prepare("INSERT INTO ".$table." (".$sum_column.") VALUES(".$sum_post.") ");
+                // INSERT命令を実行
+                $stt->execute();
+            } catch(PDOException $e) {
+                die("エラーメッセージ：{$e->getMessage()}");
+            }
+        }
+
     // テーブル名、追加したい値、追加したい値の型
-    function ma($table, $column, $post, $type) : void {
+    function ma_return ($table, $column, $post, $type) {
         try {
             // データベース接続
             include "MC-01.php";
@@ -34,6 +73,10 @@ class MA {
             $stt = $db->prepare("INSERT INTO ".$table." (".$sum_column.") VALUES(".$sum_post.") ");
             // INSERT命令を実行
             $stt->execute();
+
+            // ユーザIDを取得
+            $id = $db->lastINsertID();
+            return $id;
         } catch(PDOException $e) {
             die("エラーメッセージ：{$e->getMessage()}");
         }
