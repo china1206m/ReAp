c138dcbab1997244ffe318a3feaca42971e2a373
gackt <gackt5251@gmail.com>
2023-01-07 20:19:16 +0900

    カラム名を指定できるようにした

--- module/MA.php
+++ module/MA.php
@@ -1,40 +1,37 @@
 <?php
-
 class MA {
-
     // テーブル名、追加したい値、追加したい値の型
-    function ma($table, $post, $type) : void {
+    function ma($table, $column, $post, $type) : void {
         try {
-            include "MC-01.php";
-
             // データベース接続
+            include "MC-01.php";
             $db = getDB();
 
-            $sum; // 最終挿入値
+            $sum_column = null; // 最終挿入値column
+            $sum_post= null; // 最終挿入値post
 
             for($i = 0; $i < count($post); $i++) {
-                
                 // 複数挿入の場合
                 if($i >= 1){
-                    $sum = $sum.",";
+                    $sum_column = $sum_column.",";
+                    $sum_post = $sum_post.",";
                 }
+                // column
+                $sum_column = $sum_column.$column[$i];
                 // 型調べ
                 // INSERT
                 if($type[$i] == 0) {
-                    $sum = $sum.$post[$i];
-
+                    $sum_post = $sum_post.$post[$i];
                     // CHAR
                 } else if($type[$i] == 1) {
-                    $sum = $sum."'".$post[$i]."'";
-
+                    $sum_post = $sum_post."'".$post[$i]."'";
                     // NCHAR
                 } else {
-                    $sum = $sum."N'".$post[$i]."'";
+                    $sum_post = $sum_post."N'".$post[$i]."'";
                 }
             }
             // SQL文をセット
-            $stt = $db->prepare("INSERT INTO ".$table." VALUES(".$sum.")");
-    
+            $stt = $db->prepare("INSERT INTO ".$table." (".$sum_column.") VALUES(".$sum_post.") ");
             // INSERT命令を実行
             $stt->execute();
         } catch(PDOException $e) {
