2f997737cb1bf057f7e0e25f7dfa1a7a4504b360
nakagoshirin <240346v@ugs.kochi-tech.ac.jp>
2023-01-10 18:48:36 +0900

    AC8

---
+++ User/php/U-AC8.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang = "ja">
+    <head>
+        <title>U-AC8</title>
+        <meta charset = "UTF-8">
+        <link rel="stylesheet" href = "button.css">
+        <link rel="stylesheet" href = "U-AC8.css">
+    </head>
+    <body>
+        <div align="center">
+            <div class = "roundbox">
+                <p>
+                <font size="+4">
+                    アカウント消去規約
+                </font><br>
+    
+                <font size="+2">
+                    1.個人情報の取り扱いについて
+                </font>
+                </p>
+            </div><br>
+
+            <form action="U-AC2.html" class="button-only">
+                同意します
+            </form>
+    
+            <form action="U-AC1.html" class="button-only">
+                同意しません
+            </form>
+       
+        
+        </div>
+    </body>
+
+</html> 
