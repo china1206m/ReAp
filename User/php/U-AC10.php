7b50bc283108d17bf781b9dbec34f90b033f0e05
nakagoshirin <240346v@ugs.kochi-tech.ac.jp>
2023-01-10 19:41:09 +0900

    AC10

---
+++ User/php/U-AC10.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang = "ja">
+    <head>
+        <title>U-AC10</title>
+        <meta charset = "UTF-8">
+        <link rel="stylesheet" href = "U-AC10.css">
+    </head>
+
+    <body>
+        <button type="button" class="button_back" onclick="history.back()"><h3 class="button_back">＜</h3></button>
+
+        <div align="center">
+            <font size="+4">
+                都道府県一覧
+            </font><br>
+            <h2>
+                選択してください
+            </h2>
+            
+
+            <div class="box-tihou">
+                <h2 style="text-align:left">
+                    北海道地方
+                </h2>
+    
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    北海道
+                </button>
+            </div>
+    
+            <div class="box-tihou">
+                <h2 style="text-align:left">
+                    東北地方
+                </h2>
+    
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    青森県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    岩手県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    宮城県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    秋田県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    山形県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    福島県
+                </button>
+            </div>
+    
+            <div class="box-tihou">
+                <h2 style="text-align:left">
+                    関東地方
+                </h2>
+    
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    茨城県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    栃木県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    群馬県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    埼玉県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    千葉県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    東京都
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    神奈川県
+                </button>
+                
+            </div>
+    
+            <div class="box-tihou">
+                <h2 style="text-align:left">
+                    中部地方
+                </h2>
+    
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    新潟県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    富山県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    石川県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    福井県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    山梨県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    長野県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    岐阜県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    静岡県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    愛知県
+                </button>
+            </div>
+            
+            <div class="box-tihou">
+                <h2 style="text-align:left">
+                    近畿地方
+                </h2>
+    
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    三重県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    滋賀県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    京都府
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    大阪府
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    兵庫県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    奈良県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    和歌山県
+                </button>
+            </div>
+    
+            <div class="box-tihou">
+                <h2 style="text-align:left">
+                    中国地方
+                </h2>
+    
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    鳥取県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    島根県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    岡山県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    広島県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    山口県
+                </button>
+            </div>
+    
+            <div class="box-tihou">
+                <h2 style="text-align:left">
+                    四国地方
+                </h2>
+    
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    徳島県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    香川県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    愛媛県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    高知県
+                </button>
+            </div>
+    
+            <div class="box-tihou">
+                <h2 style="text-align:left">
+                    九州、沖縄地方
+                </h2>
+    
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    福岡県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    佐賀県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    長崎県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    熊本県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    大分県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    宮崎県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    鹿児島県
+                </button>
+                <button type="button" onclick="location.href='U-AC11.html'" class="box-tdhk">
+                    沖縄県
+                </button>
+            </div>
+
+        </div>
+
+        
+
+    </body>
+</html>
