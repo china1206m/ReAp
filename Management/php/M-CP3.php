72054023e533ea8eada18a0f2b1d17bdea1fab9d
china1206 <240371i@ugs.kochi-tech.ac.jp>
2023-01-11 18:46:36 +0900

    変更点を訂正

--- Management/php/M-CP3.php
+++ Management/php/M-CP3.php
@@ -13,17 +13,18 @@
 
     
 <div class="yoko-narabi">
-<form action="E-AC4.php" method="POST">
+<form action="E-CP4.php" method="POST">
     <ul id="coupon_list1">
-        
+        <li>
+            
     </ul>
 </form> 
 <form action="E-AC4.php" method="POST">
     <ul id="coupon_list2">
         
+        
     </ul>
 </form>
-</div>
 
  </main>
 
@@ -39,54 +40,116 @@
 </aside>
 
 <script>
+//nは表示数
+    var n=8;
     // phpで文字列に改行を入れて作成する　下のcountry,shopはphpで作成するもの
-  var country = ['店名１', '店名２', '店名３', '店名４', '店名５'];
-  var shop = ['内容１', '内容２', '内容３', '内容４', '内容５'];
+  var day = ['期限１', '期限２', '期限３', '期限４', '期限５'];
+  var shop = ['店名１', '店名２', '店名３', '店名４', '店名５'];
+  var content = ['内容１', '内容２', '内容３', '内容４', '内容５'];
 
 var ul1 = document.getElementById("coupon_list1");
-for (var count = 0; count < 3; count=count+2) {
+for (var count = 0; count < n; count=count+2) {
     // li要素を作成
     var li1 = document.createElement('li');
 
     var button1 = document.createElement('button');
-    button1.classList.add("box1");
+    button1.classList.add("coupon");
+
+    var div_left = document.createElement('div');
+    div_left.classList.add("left");
+
+    var div_shop = document.createElement('div');
+    div_shop.classList.add("shop");
+
+    var div_price = document.createElement('div');
+    div_price.classList.add("price");
+
+    var div_right = document.createElement('div');
+    div_right.classList.add("right");
+
+    var div_date = document.createElement('div');
+    div_date.classList.add("date");
+    div_date.innerText = "有効期限";
+
 
     // テキスト情報を作成
-    var text = document.createTextNode(country[count]);
-    var br = document.createElement('br');
     var shopname = document.createTextNode(shop[count]);
+    var date = document.createTextNode(day[count]);
+    var cont = document.createTextNode(content[count]);
+    var br1 = document.createElement('br');
+    var br2 = document.createElement('br');
+    var br3 = document.createElement('br');
+    
 
     // ul要素に追加
-    li1.appendChild(button1);
-    button1.appendChild(text);
-    button1.appendChild(br);
-    button1.appendChild(shopname);
     ul1.appendChild(li1);
+    li1.appendChild(button1);
+    button1.appendChild(div_left);
+    div_left.appendChild(div_shop);
+    div_shop.appendChild(shopname);
+    div_left.appendChild(div_price);
+    div_price.appendChild(br1);
+    div_price.appendChild(br2);
+    div_price.appendChild(cont);
+    button1.appendChild(div_right);
+    div_right.appendChild(div_date);
+    div_date.appendChild(br3);
+    div_date.appendChild(date);
+   
 }
 
 var ul2 = document.getElementById("coupon_list2");
-for (var count = 1; count < 3; count=count+2) {
+for (var count = 1; count < n; count=count+2) {
     // li要素を作成
-    var li2 = document.createElement('li');
+    var li1 = document.createElement('li');
 
-    var button2 = document.createElement('button');
-    button2.classList.add("box2");
+    var button1 = document.createElement('button');
+    button1.classList.add("coupon");
+
+    var div_left = document.createElement('div');
+    div_left.classList.add("left");
+
+    var div_shop = document.createElement('div');
+    div_shop.classList.add("shop");
+
+    var div_price = document.createElement('div');
+    div_price.classList.add("price");
+
+    var div_right = document.createElement('div');
+    div_right.classList.add("right");
+
+    var div_date = document.createElement('div');
+    div_date.classList.add("date");
+    div_date.innerText = "有効期限";
+
 
     // テキスト情報を作成
-    var text = document.createTextNode(country[count]);
-    var br = document.createElement('br');
     var shopname = document.createTextNode(shop[count]);
+    var date = document.createTextNode(day[count]);
+    var cont = document.createTextNode(content[count]);
+    var br1 = document.createElement('br');
+    var br2 = document.createElement('br');
+    var br3 = document.createElement('br');
+    
 
     // ul要素に追加
-    li2.appendChild(button2);
-    button2.appendChild(text);
-    button2.appendChild(br);
-    button2.appendChild(shopname);
-    ul2.appendChild(li2);
+    ul2.appendChild(li1);
+    li1.appendChild(button1);
+    button1.appendChild(div_left);
+    div_left.appendChild(div_shop);
+    div_shop.appendChild(shopname);
+    div_left.appendChild(div_price);
+    div_price.appendChild(br1);
+    div_price.appendChild(br2);
+    div_price.appendChild(cont);
+    button1.appendChild(div_right);
+    div_right.appendChild(div_date);
+    div_date.appendChild(br3);
+    div_date.appendChild(date);
 }
 
 
-  // -->
+  
 
 </script>
 </body>
