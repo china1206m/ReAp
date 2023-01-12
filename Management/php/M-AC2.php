6d47c3279c9fa3ea3fe94ad5bbda2dc9d586c5f0
nakagoshirin <240346v@ugs.kochi-tech.ac.jp>
2023-01-12 12:18:38 +0900

    M-AC2

---
+++ Management/php/M-AC2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>画面ID M-AC2</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="M_menu.css" type="text/css">
+  <link rel="stylesheet" href="M-AC2.css" type="text/css">
+  
+</head>
+
+<body bgcolor="#f0f8ff" >
+    <center>
+        <div class="box1">
+            <font size="+3">
+                ログアウトしますか？
+            </font>
+                <button  class="sou" type="button" onclick="location.href='M-AC1.php'">
+                    はい
+                </button>
+                <button type="button" class="sou" onclick="location.href='M-AC2.php'">
+                    いいえ
+                </button>
+        </div>       
+    </center>
+</body>
+</html>
