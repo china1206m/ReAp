99a992dc94558cf5ce883edeba2a809c069c2515
gackt <gackt5251@gmail.com>
2023-01-12 23:02:48 +0900

    php処理追加

--- User/php/U-HK12.php
+++ User/php/U-HK12.php
@@ -1,3 +1,34 @@
+<?php
+/* セッション開始 */
+session_start();
+ 
+/* POSTで送信されている */
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $_SESSION['user_id'] = 1;
+    /* データ挿入 */
+    // 呼び出し
+    include "MA.php";
+    // addインスタンス生成
+    $add = new MA();
+
+    // 入力したいカラム名を指定
+    $column = ['user_id','report_content'];
+    
+    // 入力された値をpost配列に格納
+    $post = [$_SESSION['user_id'], $_POST['report_content']];
+
+    // 入力された値の型を定義
+    $type = [0,1];
+
+    // 引数としてテーブル名、追加する値、追加する値の型 返り値としてID
+    $_SESSION['user_report_id'] = $add->ma_return("user_report",$column, $post, $type);
+
+    header('Location:U-HK7.php');
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -14,15 +45,15 @@
             <h2>通報理由</h2>
         </div>
     
-        <form action="U-HK1.php" method="POST" name="reportForm" onSubmit="return check();">
+        <form action="" method="POST" name="reportForm" onSubmit="return check();">
             <div class="reason-select" align="center">
                 <div class="button-box">
                     <div class="reason" style="max-width:650px;">
-                        <input type="radio" name="report_content" value="1" id="hate"><label for="hate">単に気に入らない</label>
-                        <input type="radio" name="report_content" value="2" id="spam"><label for="spam">スパムである</label>
-                        <input type="radio" name="report_content" value="3" id="lie"><label for="lie">虚偽の情報である</label>
-                        <input type="radio" name="report_content" value="4" id="scam"><label for="scam">詐欺である</label>
-                        <input type="radio" name="report_content" value="5" id="discriminatory"><label for="discriminatory">差別的発言である</label>
+                        <input type="radio" name="report_content" value="単に気に入らない" id="hate"><label for="hate">単に気に入らない</label>
+                        <input type="radio" name="report_content" value="スパムである" id="spam"><label for="spam">スパムである</label>
+                        <input type="radio" name="report_content" value="虚偽の情報である" id="lie"><label for="lie">虚偽の情報である</label>
+                        <input type="radio" name="report_content" value="詐欺である" id="scam"><label for="scam">詐欺である</label>
+                        <input type="radio" name="report_content" value="差別的発言である" id="discriminatory"><label for="discriminatory">差別的発言である</label>
                     </div>
                     <button class="button-only">決定</button>
                 </div>
