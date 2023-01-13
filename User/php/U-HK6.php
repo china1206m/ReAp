630fa8e4623cd5a54c099ccf1d2e5a036b98e21a
240285kkk <andurkt@icloud.com>
2023-01-13 17:34:24 +0900

    kaiketu

--- User/php/U-HK6.php
+++ User/php/U-HK6.php
@@ -1,47 +1,74 @@
+<?php
+
+include "MG.php";
+
+$db = getDB();
+$sql = "SELECT * FROM plan ORDER BY post_date DESC LIMIT 50";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+
+$count1 = $stmt->rowCount();
+
+$plan = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+
 <!DOCTYPE html>
 <html>
-<head>
-  <title>U-HK6</title>
-  <meta charset=”utf-8″>
-  <link rel="stylesheet" href="U-HK6.css" type="text/css">
-  <link rel="stylesheet" href="U-menu.css" type="text/css">
-</head>
-<body>
-  <main id="main">
-    <div align="right">
-      <button class="btn" href="U-HK2.php"><img src="crown.png"></button>
-      <button class="btn" href="U-HK5.html"><img src="post.png"></button>    
-      <button class="btn" href="U-HK3.php"><img src="search.png"></button>
-    </div>
+  <head>
+    <title>U-HK6</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="U-HK6.css" type="text/css">
+    <link rel="stylesheet" href="U-menu.css" type="text/css">
+  </head>
+  <body>
+    <main id="main">
+      <div align="right">
+        <button class="btn" href="U-HK2.php"><img src="crown.png"></button>
+        <button class="btn" href="U-HK5.html"><img src="post.png"></button>    
+        <button class="btn" href="U-HK3.php"><img src="search.png"></button>
+      </div>
 
-    <ul id="planlist">
-    </ul>
-  </main>
+      <ul id="planlist">
+      </ul>
+    </main>
 
-<aside id="sub">
-  <ul class="menu">
-      <li class="menu-list"><a class="menu-button" href="U-HK1.php"><img class="menu_img" src="U-menu-home.png" >　ホーム</a></li><br>
-      <li class="menu-list"><a class="menu-button" href="U-PL1.php"><img class="menu_img" src="U-menu-place.png">　名所</a></li><br>
-      <li class="menu-list"><a class="menu-button" href="U-EV1.php"><img class="menu_img" src="U-menu-event.png">　イベント</a></li><br>
-      <li class="menu-list"><a class="menu-button" href="U-FV1.php"><img class="menu_img" src="U-menu-favorite.png">　お気に入り</a></li><br>
-      <li class="menu-list"><a class="menu-button" href="U-AC3.php"><img class="menu_img" src="U-menu-acount.png">　アカウント</a></li><br>
-    </ul>
-</aside>
+    <aside id="sub">
+      <ul class="menu">
+        <li class="menu-list"><a class="menu-button" href="U-HK1.php"><img class="menu_img" src="U-menu-home.png" >　ホーム</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="U-PL1.php"><img class="menu_img" src="U-menu-place.png">　名所</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="U-EV1.php"><img class="menu_img" src="U-menu-event.png">　イベント</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="U-FV1.php"><img class="menu_img" src="U-menu-favorite.png">　お気に入り</a></li><br>
+        <li class="menu-list"><a class="menu-button" href="U-AC3.php"><img class="menu_img" src="U-menu-acount.png">　アカウント</a></li><br>
+      </ul>
+    </aside>
 
-<script>
-    var country = ['日本', 'アメリカ', 'イギリス', 'ロシア', 'フランス'];
-    var ul = document.getElementById("planlist");
-    for (var count = 0; count < 6; count++) {
-        var li = document.createElement('li');
-        li.classList.add("home-list");
+    <script>
+      var country = ['日本', 'アメリカ', 'イギリス', 'ロシア', 'フランス'];
+      var ul = document.getElementById("planlist");
+      <?php 
 
-        var p = document.createElement('p');
+        for ($i = 0; $i < $count1; $i++) :  
 
-        //ランキング情報のための四角追加
-        var div_planlist = document.createElement('div');
-        div_planlist.classList.add("planlist_information");
+          $user_id = $plan[$i]['user_id']; 
+          $db = MG_01($user_id,"","","","","","","","");
+          $user = $db->fetchAll(PDO::FETCH_ASSOC);
 
-        //投稿日の追加
+          $plan_id = $plan[$i]['plan_id']; 
+          $db = MG_05("",$plan_id,"","","","","","","");
+          $plan_detail = $db->fetchAll(PDO::FETCH_ASSOC);
+      ?>
+      var li = document.createElement('li');
+      li.classList.add("home-list");
+
+      var p = document.createElement('p');
+
+      //ランキング情報のための四角追加
+      var div_planlist = document.createElement('div');
+      div_planlist.classList.add("planlist_information");
+
+      //投稿日の追加
       var div_right = document.createElement('div');
       div_right.classList.add("right");
       div_right.innerText = "<?php print($plan[$i]['post_date']); ?>"
@@ -50,92 +77,96 @@
       var div_pre = document.createElement('div');
       div_pre.innerText = "都道府県"
 
-        //アイコンと題名の横並びのためのクラス追加
-        var div_yoko = document.createElement('div');
-        div_yoko.classList.add("yoko");
+      //アイコンと題名の横並びのためのクラス追加
+      var div_yoko = document.createElement('div');
+      div_yoko.classList.add("yoko");
 
-        //アイコン追加
-        var img = document.createElement('img');
-        img.classList.add("circle");
-        img.src = 'monky.png';
-        img.align = 'left'
-        img.alt = 'アイコン'
+      //アイコン追加
+      var img = document.createElement('img');
+      img.classList.add("circle");
+      img.src = 'monky.png';
+      img.align = 'left'
+      img.alt = '<?php //print($user[0]['user_name']); ?>'
         
-        //題名追加
-        var div_title = document.createElement('div');
-        div_title.classList.add("title");
-        div_title.innerHTML = "題名";
+      //題名追加
+      var div_title = document.createElement('div');
+      div_title.classList.add("title");
+      div_title.innerHTML = "<?php print($plan[$i]['plan_title']); ?>";
 
-        var br = document.createElement('br');
+      var br = document.createElement('br');
 
-        //条件追加
-        var p_who = document.createElement('p');
-        p_who.classList.add("condition");
-        p_who.innerHTML = "誰と"
-        var p_cost = document.createElement('p');
-        p_cost.classList.add("condition");
-        p_cost.innerHTML = "費用"
-        var p_day = document.createElement('p');
-        p_day.classList.add("condition");
-        p_day.innerHTML = "何日"
+      //条件追加
+      var p_who = document.createElement('p');
+      p_who.classList.add("condition");
+      p_who.innerHTML = "<?php print($plan[$i]['plan_who']); ?>"
+      var p_cost = document.createElement('p');
+      p_cost.classList.add("condition");
+      p_cost.innerHTML = "<?php print($plan[$i]['plan_cost']); ?>"
+      var p_day = document.createElement('p');
+      p_day.classList.add("condition");
+      p_day.innerHTML = "<?php print($plan[$i]['plan_day']); ?>"
 
-        //olの追加
-        var ol = document.createElement('ol');
+      //olの追加
+      var ol = document.createElement('ol');
 
-        //ol内のliの追加
-        var li_ol = document.createElement('li');
+      //ol内のliの追加
+      var li_ol = document.createElement('li');
 
-        //場所の四角の追加
-        var div_home = document.createElement('div');
-        div_home.classList.add("home_information");
+      //場所の四角の追加
+      var div_home = document.createElement('div');
+      div_home.classList.add("home_information");
 
-        //場所名追加
-        var p_planname = document.createElement('p');
-        p_planname.classList.add("plan_content");
-        p_planname.innerHTML = "場所名"
+      //場所名追加
+      var p_planname = document.createElement('p');
+      p_planname.classList.add("plan_content");
+      p_planname.innerHTML = "<?php print($plan_detail[0]['plan_place']); ?>"
 
-        //本文内容追加
-        var p_content = document.createElement('p');
-        p_content.classList.add("plan_content");
-        p_content.innerHTML = "本文内容"
+      //本文内容追加
+      var p_content = document.createElement('p');
+      p_content.classList.add("plan_content");
+      p_content.innerHTML = "<?php print($plan_detail[0]['plan_content']); ?>"
 
-        //滞在時間追加
-        var p_time = document.createElement('p');
-        p_time.innerHTML = "滞在時間"
-        p_time.classList.add("plan_content");
+      //滞在時間追加
+      var p_time = document.createElement('p');
+      p_time.innerHTML = "<?php print($plan_detail[0]['stay_time_minute']); ?>"
+      p_time.classList.add("plan_content");
 
-        //移動時間追加
-        var p_travel = document.createElement('p');
-        p_travel.classList.add("travel_time");
-        p_travel.innerHTML = "移動時間"
+        
 
-        // もっと見るを作成
-        var a = document.createElement('a');
-        a.classList.add("more-see");
-        a.href = "U-HK7.php";
-        a.innerText = "...もっと見る";
+      //移動時間追加
+      var p_travel = document.createElement('p');
+      p_travel.classList.add("travel_time");
+      p_travel.innerHTML = "<?php print($plan_detail[0]['travel_time_minute']); ?>"
 
-        ul.appendChild(li);
-        li.appendChild(p);
-        li.appendChild(div_planlist);
-        div_planlist.appendChild(div_right);
-        div_planlist.appendChild(div_yoko);
-        div_planlist.appendChild(div_pre);
-        div_yoko.appendChild(img);
-        div_yoko.appendChild(div_title);
-        div_planlist.appendChild(br);
-        div_planlist.appendChild(br);
-        div_planlist.appendChild(p_who);
-        div_planlist.appendChild(p_cost);
-        div_planlist.appendChild(p_day);
-        div_planlist.appendChild(ol);
-        ol.appendChild(li_ol);
-        li_ol.appendChild(div_home);
-        div_home.appendChild(p_planname);
-        div_home.appendChild(p_content);
-        div_home.appendChild(p_time);
-        div_planlist.appendChild(p_travel);
-        div_plnalist.appendChild(a);   
-    }
-</script>
-</body>
+      // もっと見るを作成
+      var a = document.createElement('a');
+      a.classList.add("more-see");
+      a.href = "U-HK7.php";
+      a.innerText = "...もっと見る";
+
+
+      ul.appendChild(li);
+      li.appendChild(p);
+      li.appendChild(div_planlist);
+      div_planlist.appendChild(div_yoko);
+      div_yoko.appendChild(img);
+      div_yoko.appendChild(div_title);
+      div_planlist.appendChild(br);
+      div_planlist.appendChild(br);
+      div_planlist.appendChild(p_who);
+      div_planlist.appendChild(p_cost);
+      div_planlist.appendChild(p_day);
+      div_planlist.appendChild(ol);
+      ol.appendChild(li_ol);
+      li_ol.appendChild(div_home);
+      div_home.appendChild(p_planname);
+      div_home.appendChild(p_content);
+      div_home.appendChild(p_time);
+      div_planlist.appendChild(p_travel);
+      div_planlist.appendChild(a);
+
+        <?php endfor; ?>
+
+    </script>
+  </body>
+</html>
