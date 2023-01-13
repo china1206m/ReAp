d576cb69938902839d4c5fb563313ea67731c14a
IshiiSuzuka <suzuka.222281@gmail.com>
2023-01-13 13:57:11 +0900

    あげなおし

--- User/php/U-EV1.php
+++ User/php/U-EV1.php
@@ -1,19 +1,3 @@
-<?php
-
-include "MG.php";
-
-$db = getDB();
-$sql = "SELECT * FROM event ORDER BY post_date ASC LIMIT 50";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-
-$count1 = $stmt->rowCount();
-
-$event = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -35,7 +19,7 @@ $event = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <ul class="menu">
       <li class="menu-list"><a class="menu-button" href="U-HK1.php"><img class="menu_img" src="U-menu-home.png" >　ホーム</a></li><br>
       <li class="menu-list"><a class="menu-button" href="U-PL1.php"><img class="menu_img" src="U-menu-place.png">　名所</a></li><br>
-      <li lass="menu-list"><a class="menu-button" href="U-EV1.php"><img class="menu_img" src="U-menu-event.png">　イベント</a></li><br>
+      <li class="menu-list"><a class="menu-button" href="U-EV1.php"><img class="menu_img" src="U-menu-event.png">　イベント</a></li><br>
       <li class="menu-list"><a class="menu-button" href="U-FV1.php"><img class="menu_img" src="U-menu-favorite.png">　お気に入り</a></li><br>
       <li class="menu-list"><a class="menu-button" href="U-AC3.php"><img class="menu_img" src="U-menu-acount.png">　アカウント</a></li><br>
     </ul>
@@ -44,16 +28,7 @@ $event = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <script>
     var country = ['日本', 'アメリカ', 'イギリス', 'ロシア', 'フランス'];
     var ul = document.getElementById("event-list");
-
-    <?php
-
-      for ($i = 0; $i < $count1; $i++) :
-
-        $eventuser_id = $event[$i]['eventuser_id']; 
-        $db = MG_02($eventuser_id,"","","","","","","","","");
-        $eventuser = $db->fetchAll(PDO::FETCH_ASSOC);
-
-    ?>
+    for (var count = 0; count < 6; count++) {
         var li = document.createElement('li');
         li.classList.add("list");
   
@@ -66,7 +41,7 @@ $event = $stmt->fetchAll(PDO::FETCH_ASSOC);
         img.classList.add("circle");
         img.src = 'monky.png';
         img.align = 'left'
-        img.alt = '<?php //print($eventuser[0]['eventuser_name']); ?>'
+        img.alt = 'アイコン'
   
         //アイコンと題名の横並びのためのクラス追加
         var div_yoko = document.createElement('div');
@@ -75,14 +50,14 @@ $event = $stmt->fetchAll(PDO::FETCH_ASSOC);
         //題名追加
         var div_title = document.createElement('div');
         div_title.classList.add("title");
-        div_title.innerHTML = "<?php print($event[$i]['event_title']); ?>";
+        div_title.innerHTML = "題名";
   
         var br = document.createElement('br');
   
         //条件追加
         var p = document.createElement('p');
         p.classList.add("content");
-        p.innerHTML = "<?php print($event[$i]['event_content']); ?>"
+        p.innerHTML = "本文"
 
         // もっと見るを作成
         var a = document.createElement('a');
@@ -99,9 +74,7 @@ $event = $stmt->fetchAll(PDO::FETCH_ASSOC);
         div_ranking.appendChild(br);
         div_ranking.appendChild(p);
         div_ranking.appendChild(a);
-    
-
-        <?php endfor; ?>
+    }
   </script>
 </body>
 </html>
