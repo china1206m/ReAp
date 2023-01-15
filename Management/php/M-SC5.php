c15f926904928fd6e515a35d92e9b80e6da39180
IshiiSuzuka <suzuka.222281@gmail.com>
2023-01-15 13:26:13 +0900

    M-SC5.php作成

---
+++ Management/php/M-SC5.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>M-SC5</title>
+  <meta charset=”UTF-8″>
+  <link rel="stylesheet" href="M-SC5.css" type="text/css">
+  <link rel="stylesheet" href="M-menu.css" type="text/css">
+</head>
+<body bgcolor="#f0f8ff">
+  <main id="main">
+    <button type="button" class="button_back" onclick="history.back()"><h3>＜</h3></button><h3 class="button_back">検索結果</h3>
+<div>
+  <ul id="event_list">
+  </ul>
+</div>
+</main>
+
+<aside id="sub">
+    <ul>
+      <li class="menu-list"><a class="menu-button" href="M-HK1.php.html"><img src="M-menu-home.png" width="45" height="43">　ホーム</a></li><br>
+      <li class="menu-list"><a class="menu-button" href="M-PL1.php"><img src="M-menu-place.png" width="45" height="43">　名所</a></li><br>
+      <li class="menu-list"><a class="menu-button" href="M-EV1.php"><img src="M-menu-event.png" width="45" height="43">　イベント</a></li><br>
+      <li class="menu-list"><a class="menu-button" href="M-RP1.php"><img src="M-menu-report.png" width="45" height="43">　通報一覧</a></li><br>
+      <li class="menu-list"><a class="menu-button" href="M-CP1.php"><img src="M-menu-coupon.png" width="45" height="43">　クーポン</a></li><br>
+      <li class="menu-list"><a class="menu-button" href="M-UM1.php"><img src="M-menu-acount.png" width="45" height="43">　ユーザ管理</a></li><br>
+    </ul>
+</aside>
+
+<script>
+    var country = ['日本', 'アメリカ', 'イギリス', 'ロシア', 'フランス'];
+    var ul = document.getElementById("event_list");
+    
+    <?php
+
+      /*for ($i = 0; $i < $count1; $i++) :*/
+
+    ?>
+
+        var li = document.createElement('li');
+        li.classList.add("list");
+  
+        //ランキング情報のための四角追加
+        var div_eventlist = document.createElement('div');
+        div_eventlist.classList.add("all_information");
+
+        //投稿日の追加
+      var div_right = document.createElement('div');
+      div_right.classList.add("right");
+      div_right.innerText = "<?php //print($plan[$i]['post_date']); ?>投稿日時"
+
+        //アイコン追加
+        var img = document.createElement('img');
+        img.classList.add("circle");
+        img.src = 'monky.png';
+        img.align = 'left'
+        img.alt = 'アイコン'
+  
+        //アイコンと題名の横並びのためのクラス追加
+        var div_yoko = document.createElement('div');
+        div_yoko.classList.add("yoko");
+        
+        //題名追加
+        var div_title = document.createElement('div');
+        div_title.classList.add("title");
+        div_title.innerHTML = "<?php //print($event[$i]['event_title']); ?>タイトル";
+  
+        var br = document.createElement('br');
+
+        //都道府県の追加
+      var p_pre = document.createElement('p');
+      p_pre.classList.add("pref");
+      p_pre.innerText = "<?php //print($plan[$i]['plan_prefectures']); ?>都道府県"
+  
+        //内容追加
+        var p = document.createElement('p');
+        p.classList.add("content");
+        p.innerHTML = "<?php //print($event[$i]['event_content']); ?>ないよう"
+
+        // もっと見るを作成
+        var a = document.createElement('a');
+        a.classList.add("more-see");
+        a.href = "U-EV6.php";
+        a.innerText = "...もっと見る";
+  
+        ul.appendChild(li);
+        li.appendChild(div_eventlist);
+        div_eventlist.appendChild(div_right);
+        div_eventlist.appendChild(div_yoko);
+        div_yoko.appendChild(img);
+        div_yoko.appendChild(div_title);
+        div_eventlist.appendChild(br);
+        div_eventlist.appendChild(br);
+        div_eventlist.appendChild(p_pre);
+        div_eventlist.appendChild(p);
+        div_eventlist.appendChild(a);
+    
+        <?php //endfor; ?>
+
+  </script>
+</body>
+</html>
