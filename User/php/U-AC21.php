c7dcc8b1ee5288c9a232d2b98556d562008a27eb
nakagoshirin <240346v@ugs.kochi-tech.ac.jp>
2023-01-17 18:19:46 +0900

    遷移先の変更

--- User/php/U-AC21.php
+++ User/php/U-AC21.php
@@ -10,13 +10,13 @@
   <main id="main">
     <button type="submit" class="button_back" onclick="history.back()"><h3>＜</h3><h3 class="button_back"></h3></button>
 
-    <form action='U-AC24.php U-AC18.php' method="POST" enctype="multipart/form-data">
+    
 
     <div align="right">
-        <button type="submit" onclick="location.href='U-AC24.php'" class="solid">
+        <button type="type" onclick="location.href='U-AC24.php'" class="solid">
             <img src="hensyu.png" alt="編集" width="50" height="50">
         </button>
-        <button type="submit" onclick="location.href='U-AC18.php'" class="solid">
+        <button type="type" onclick="location.href='U-AC18.php'" class="solid">
             <img src="gomibako.png" alt="ゴミ箱" width="50" height="50">
         </button>
     </div>
@@ -36,7 +36,6 @@
         </div>
       </li>
     </ul>
-  </form>
   </main>
 
 <aside id="sub">
