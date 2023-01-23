82116470b603fa731f5fb4fc1e56e01f8dbecf49
gackt <gackt5251@gmail.com>
2023-01-23 09:10:06 +0900

    php処理修正

--- EventUser/php/E-CP2.php
+++ EventUser/php/E-CP2.php
@@ -1,11 +1,11 @@
 <?php
-/* セッション開始 */
 session_start();
 
-/* POSTで送信されている */
+// POSTで送信されている 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $_SESSION['coupon_search'] = $_POST['coupon_search'];
+    $_SESSION['coupon_name'] = $_POST['coupon_name'];
     $_SESSION['coupon_prefectures'] = $_POST['coupon_prefectures'];
     $_SESSION['coupon_place'] = $_POST['coupon_place'];
     $_SESSION['coupon_deadline'] = $_POST['coupon_deadline'];
