eb8625466e673698a781d014034a33bf6954cc38
china1206 <240371i@ugs.kochi-tech.ac.jp>
2023-01-25 13:31:18 +0900

    オーバレイ上の表示変更

--- EventUser/php/E-CP6.php
+++ EventUser/php/E-CP6.php
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         
             <tr>
             <td>
-            <select name="coupon_prefectures" class="prefectures-shop" required>
+            <select id="pre" name="coupon_prefectures" class="prefectures-shop" required>
             <option value="">都道府県を選択</option>
             <option value="北海道">北海道</option>
             <optgroup label="東北">
@@ -127,7 +127,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </select>
             </td>
         
-            <td><input type="text" class="prefectures-shop" name="coupon_place" maxlength="30" placeholder="30文字以内" required></td>
+            <td><input type="text" id="shop" class="prefectures-shop" name="coupon_place" maxlength="30" placeholder="30文字以内" required></td>
             </tr>
         </table>
     
@@ -135,30 +135,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <input type="date" id="date" name="coupon_deadline" class="day" required>
 
         <p><label for="name">クーポン名<span class="require">必須</span></label></p>
-        <input type="text" name="coupon_name" class="prefectures-shop" maxlength="30" placeholder="20文字以内" required>
+        <input type="text" id="name" name="coupon_name" class="prefectures-shop" maxlength="30" placeholder="20文字以内" required>
     
         <p><label for="shop">詳細<span class="require">必須</span></label></p>
-        <textarea class="tarea" name="coupon_content" maxlength="1000" placeholder="1000文字以内" required></textarea>
+        <textarea class="tarea" id="cont" name="coupon_content" maxlength="1000" placeholder="1000文字以内" required></textarea>
     
      
         
 
       <center>
-        <button id="open-btn" class="overlay-event">発行する</button>
+        <button id="open-btn" class="overlay-event" onclick="make()">発行する</button>
       </center>
     </form>  
    
       <div style="display: block;" id="overlay">
         <div class="flex">
           <div id="overlay-inner">
-            <hr class="top">
-            都道府県
+          <hr class="top">
+            　都道府県：<input disabled class="admit"  type="text" id="admit_1" required>
             <hr>
-            店名
+            　　　店名：<input disabled class="admit" type="text" id="admit_2" required>
             <hr>
-            使用期限
+            　使用期限：<input disabled class="admit" type="text" id="admit_3" required>
             <hr>
-            詳細
+            クーポン名：<input disabled class="admit" type="text" id="admit_4" required>
+            <hr>
+            詳細<br><input disabled class="admit" type="text" id="admit_5" required>
             <hr class="under">
           <!--idはデザイン-->
           <button id="close-btn1" class="close" value="" disabled>はい</button>
@@ -239,7 +241,34 @@ function disp() {
   </script>
 
 
+<script>
+ function make(){
+    let info1_pre = document.getElementById('pre');
+    var info2_pre = info1_pre.value
+    let out_pre = document.getElementById('admit_1');
+    out_pre.value = info2_pre;
 
+    let info1_shop = document.getElementById('shop');
+    var info2_shop = info1_shop.value
+    let out_shop = document.getElementById('admit_2');
+    out_shop.value = info2_shop;
+
+    let info1_day = document.getElementById('date');
+    var info2_day = info1_day.value
+    let out_day = document.getElementById('admit_3');
+    out_day.value = info2_day;
+
+    let info1_name = document.getElementById('name');
+    var info2_name = info1_name.value
+    let out_name = document.getElementById('admit_4');
+    out_name.value = info2_name;
+
+    let info1_cont = document.getElementById('cont');
+    var info2_cont = info1_cont.value
+    let out_cont = document.getElementById('admit_5');
+    out_cont.value = info2_cont;
+ }
+  </script>
 
 
 </body>
