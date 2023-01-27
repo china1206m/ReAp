9453e92dc1f314513bc69c0a478041dc7243a73d
china1206 <240371i@ugs.kochi-tech.ac.jp>
2023-01-27 18:06:24 +0900

    検索画面に変更

--- User/php/U-AC10.php
+++ User/php/U-AC10.php
@@ -1,254 +1,124 @@
 <?php
-// セッション開始
 session_start();
-include "MGvG.php";
-$post = [null, null, null, null, null, null, $_SESSION['user_id']];
-$db = MG("user", $post);
-$coupon = $db->fetchAll(PDO::FETCH_ASSOC);
-$coupn_count = $coupon[0]['coupon_can_get'];
-if($coupn_count == 0) {
-   header('Location:U-AC9.html');
+
+// POSTで送信されている 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $_SESSION['coupon_search'] = $_POST['coupon_search'];
+    $_SESSION['coupon_name'] = $_POST['coupon_name'];
+    $_SESSION['coupon_prefectures'] = $_POST['coupon_prefectures'];
+    $_SESSION['coupon_place'] = $_POST['coupon_place'];
+    $_SESSION['coupon_deadline'] = $_POST['coupon_deadline'];
+    $_SESSION['plan_cost'] = $_POST['plan_cost'];
+
+    // 画面遷移　クーポン検索結果画面
+    header('Location:E-CP3.php');
     exit;
-}
-// POSTで送信されたら
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-  $_SESSION['coupon_prefectures'] = $_POST['ccoupon_prefectures'];
-  // 取得クーポン一覧画面
-  header('Location:U-AC11.php');
-  exit;
 }
 ?>
 
 <!DOCTYPE html>
-<html lang = "ja">
-    <head>
-        <title>U-AC10</title>
-        <meta charset = "UTF-8">
-        <link rel="stylesheet" href = "U-AC10.css">
-        <link rel="stylesheet" href="U-menu.css" type="text/css">
-    </head>
+<html>
+<head>
+  <title>画面ID U-AC10</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="U-AC10.css" type="text/css">
+  <link rel="stylesheet" href="U-menu.css" type="text/css">
+</head>
+<body>
 
-    <body>
-        <main id="main">
-        <button type="button" class="button_back" onclick="history.back()"><h3>＜</h3></button>
+<main id="main">
+    <button type="button" class="button_back" onclick="history.back()"><h3>＜</h3></button><h3 class="button_back">クーポン検索</h3>
 
-        <form action='' method="POST" enctype="multipart/form-data">
+    <form action="" method="POST" name="searchForm" onSubmit="return check();">
+    <input type="search" name="coupon_search1" class="event_search" placeholder="キーワードを入力">
+    <table>
+        <tr>
+            <td><p><label class="label-prefectures-shop" for="prefectures" >都道府県</label></p></td>
+            <td><p><label class="label-prefectures-shop" for="shop">店名</label></p></td>
+        </tr>
+    
+        <tr>
+            <td><select name="coupon_prefectures" class="prefectures-shop">
+        <option value="">都道府県を選択</option>
+        <option value="北海道">北海道</option>
+        <optgroup label="東北">
+          <option value="青森県">青森県</option>
+          <option value="秋田県">秋田県</option>
+          <option value="岩手県">岩手県</option>
+          <option value="山形県">山形県</option>
+          <option value="宮城県">宮城県</option>
+          <option value="福島県">福島県</option>
+        </optgroup>
+        <optgroup label="関東">
+          <option value="茨城県">茨城県</option>
+          <option value="栃木県">栃木県</option>
+          <option value="群馬県">群馬県</option>
+          <option value="埼玉県">埼玉県</option>
+          <option value="千葉県">千葉県</option>
+          <option value="東京都">東京都</option>
+          <option value="神奈川県">神奈川県</option>
+        </optgroup>
+        <optgroup label="中部">
+          <option value="新潟県">新潟県</option>
+          <option value="富山県">富山県</option>
+          <option value="石川県">石川県</option>
+          <option value="福井県">福井県</option>
+          <option value="山梨県">山梨県</option>
+          <option value="長野県">長野県</option>
+          <option value="岐阜県">岐阜県</option>
+          <option value="静岡県">静岡県</option>
+          <option value="愛知県">愛知県</option>
+        </optgroup>
+        <optgroup label="近畿">
+          <option value="三重県">三重県</option>
+          <option value="滋賀県">滋賀県</option>
+          <option value="京都府">京都府</option>
+          <option value="大阪府">大阪府</option>
+          <option value="兵庫県">兵庫県</option>
+          <option value="奈良県">奈良県</option>
+          <option value="和歌山県">和歌山県</option>
+        </optgroup>
+        <optgroup label="中国">
+          <option value="岡山県">岡山県</option>
+          <option value="広島県">広島県</option>
+          <option value="鳥取県">鳥取県</option>
+          <option value="島根県">島根県</option>
+          <option value="山口県">山口県</option>
+        </optgroup>
+        <optgroup label="四国">
+          <option value="徳島県">徳島県</option>
+          <option value="香川県">香川県</option>
+          <option value="愛媛県">愛媛県</option>
+          <option value="高知県">高知県</option>
+        </optgroup>
+        <optgroup label="九州沖縄">
+          <option value="福岡県">福岡県</option>
+          <option value="佐賀県">佐賀県</option>
+          <option value="長崎県">長崎県</option>
+          <option value="熊本県">熊本県</option>
+          <option value="大分県">大分県</option>
+          <option value="宮崎県">宮崎県</option>
+          <option value="鹿児島県">鹿児島県</option>
+          <option value="沖縄県">沖縄県</option>
+        </optgroup>
+      </select></td>
+    
+        <td><input type="text" class="prefectures-shop" name="coupon_place" maxlength="30"></td>
+    </tr>
+    </table>
 
-        <div align="center">
-            <font size="+4">
-                都道府県一覧
-            </font><br>
-            <h2>
-                選択してください
-            </h2>
-            
+    <p><label for="shop">使用期限</label></p>
+    <input type="date" id="date" name="coupon_deadline" class="day" value="">
 
-            <div class="box-tihou">
-                <h2 style="text-align:left">
-                    北海道地方
-                </h2>
+    <p><label for="name">クーポン名</label></p>
+    <input type="text" name="coupon_name" class="prefectures-shop" maxlength="30" placeholder="20文字以内">
     
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="北海道">
-                    北海道
-                </button>
-            </div>
-    
-            <div class="box-tihou">
-                <h2 style="text-align:left">
-                    東北地方
-                </h2>
-    
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="青森県">
-                    青森県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="岩手県">
-                    岩手県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="宮城県">
-                    宮城県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="秋田県">
-                    秋田県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="山形県">
-                    山形県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="福島県">
-                    福島県
-                </button>
-            </div>
-    
-            <div class="box-tihou">
-                <h2 style="text-align:left">
-                    関東地方
-                </h2>
-    
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="茨城県">
-                    茨城県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="栃木県">
-                    栃木県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="群馬県">
-                    群馬県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="埼玉県">
-                    埼玉県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="千葉県">
-                    千葉県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="東京都">
-                    東京都
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="神奈川県">
-                    神奈川県
-                </button>
-                
-            </div>
-    
-            <div class="box-tihou">
-                <h2 style="text-align:left">
-                    中部地方
-                </h2>
-    
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="新潟県">
-                    新潟県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="富山県">
-                    富山県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="石川県">
-                    石川県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="福井県">
-                    福井県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="山梨県">
-                    山梨県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="長野県">
-                    長野県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="岐阜県">
-                    岐阜県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="静岡県">
-                    静岡県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="愛知県">
-                    愛知県
-                </button>
-            </div>
-            
-            <div class="box-tihou">
-                <h2 style="text-align:left">
-                    近畿地方
-                </h2>
-    
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="三重県">
-                    三重県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="滋賀県">
-                    滋賀県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="京都府">
-                    京都府
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="大阪府">
-                    大阪府
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="兵庫県">
-                    兵庫県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="奈良県">
-                    奈良県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="和歌山県">
-                    和歌山県
-                </button>
-            </div>
-    
-            <div class="box-tihou">
-                <h2 style="text-align:left">
-                    中国地方
-                </h2>
-    
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="鳥取県">
-                    鳥取県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="島根県">
-                    島根県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="岡山県">
-                    岡山県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="広島県">
-                    広島県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="山口県">
-                    山口県
-                </button>
-            </div>
-    
-            <div class="box-tihou">
-                <h2 style="text-align:left">
-                    四国地方
-                </h2>
-    
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="徳島県">
-                    徳島県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="香川県">
-                    香川県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="愛媛県">
-                    愛媛県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="高知県">
-                    高知県
-                </button>
-            </div>
-    
-            <div class="box-tihou">
-                <h2 style="text-align:left">
-                    九州、沖縄地方
-                </h2>
-    
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="福岡県">
-                    福岡県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="佐賀県">
-                    佐賀県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="長崎県">
-                    長崎県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="熊本県">
-                    熊本県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="大分県">
-                    大分県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="宮城県">
-                    宮崎県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="鹿児島県">
-                    鹿児島県
-                </button>
-                <button type="submit" class="box-tdhk" name="coupon_prefectures" value="沖縄県">
-                    沖縄県
-                </button>
-            </div>
-            <p>
-                <br>
-                <br>
-                <br>
-                <br>
-            </p>
 
-        </div>
-        </form>
+    <center>
+    <button type="submit" name="coupon_search" class="button-only">検索</button>
+    </center>
+</form>
     </main>
 
     <aside id="sub">
