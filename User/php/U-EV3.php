1e0913bee9fdb710675813ed36041f3b447d2db7
china1206 <240371i@ugs.kochi-tech.ac.jp>
2023-01-29 22:28:13 +0900

    デフォルト作成

--- User/php/U-EV3.php
+++ User/php/U-EV3.php
@@ -43,7 +43,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <div class="acount_information">
       <div class="yoko">
-        <img src="E-ImageUser.php?id=<?= $eventuser[0]['eventuser_id']; ?>" align="left" alt="写真" class="circle">
+        <img src="E-ImageUser.php?id=<?= $eventuser[0]['eventuser_id']; ?>" align="left" alt="写真" class="circle1">
+
+        <!--デフォルト-->
+        <img src="castle.bmp" align="left" alt="写真" class="circle">
+        
         <div class="user_name">
           <?php print($eventuser[0]['eventuser_name']); ?>
         </div>
