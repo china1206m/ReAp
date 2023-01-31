855b4cfaa190581220617c0818af653c3cce626e
gackt <gackt5251@gmail.com>
2023-01-31 23:24:38 +0900

    戻るボタン変更

--- User/php/U-EV2.php
+++ User/php/U-EV2.php
@@ -24,11 +24,12 @@ $_SESSION['event_eventuser_id'] = $eventuser_id;
   <link rel="stylesheet" href="U-EV2.css" type="text/css">
   <link rel="stylesheet" href="U-EV2.css" type="text/css">
   <link rel="stylesheet" href="U-menu.css" type="text/css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
 
 <body>
   <main id="main">
-    <button type="button" class="button_back" onclick="history.back()"><h3>＜</h3></button>
+  <i class="fas fa-less-than less fa-3x" onclick="history.back()"></i>
 
     <div class="yoko">
       <div class="event_information">
