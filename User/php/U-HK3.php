231f14498a391bbe0219fb52a737d7e4d79ac49c
china1206 <240371i@ugs.kochi-tech.ac.jp>
2023-02-01 18:40:08 +0900

    修正

--- User/php/U-HK3.php
+++ User/php/U-HK3.php
@@ -43,6 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="U-HK3.css" type="text/css">
     <link rel="stylesheet" href="button.css" type="text/css">
     <link rel="stylesheet" href="U-menu.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <title>U-HK3</title>
 </head>
 
